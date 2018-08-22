b7d3b396407d3991dc37dd4eabc8902a35a22091
Natanael Nistor <nnistor@ggc.edu>
2018-08-22 16:27:30 -0400

    Added my name in file

--- 01-read_input_NatanaelzCOPY.sh
+++ 01-read_input_NatanaelzCOPY.sh
@@ -2,8 +2,8 @@
 # read the name of the user and print hello
 
 echo "Hello! What is your name"
-read name
-echo "Welcome, $name"
+NAME="Natanael Nistor"
+echo "Welcome, $NAME"
 
 # single quotes prevent the expansion of the variable
 echo 'Your name was stored in $name'
