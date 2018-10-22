bb9afb0031b4d3707d378ae04a5aea59bab38802
Natanael Nistor <nnistor@ggc.edu>
2018-10-21 20:04:02 -0400

    git assginment 2 second attempt for branch01

--- 03-happy.sh
+++ 03-happy.sh
@@ -14,3 +14,11 @@ fi
 
 # exercise: write a script that prints whether today is
 # the weekend or not
+
+day=$(date +"%u")
+
+if ((day > 5)); then
+   echo "WEEKEND!"        
+else
+   echo "feels bad man..."
+fi
